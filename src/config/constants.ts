6787b54cf0ac9eb521eadfd1c903cd8a990223cf
Jimmy <307400468@qq.com>
2025-04-07 20:41:20 +0800

    请求地址

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -4,7 +4,7 @@ const IS_DEV = import.meta.env.DEV
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 // export const API_BASE_URL = '/' // mock
 // export const DEV_URL = 'http://47.130.75.121:31962'
-export const DEV_URL = 'https://invite.cybercharge.cc:9091'
+export const DEV_URL = 'https://invite.cybercharge.cc'
 
 export const USER_TOKEN = 'USER_TOKEN'
 export const APP_LANGUAGE = 'APP_LANGUAGE'
